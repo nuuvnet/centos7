294d86f2d56d3823a573bc4bba3ea6487cf3e82a
Robson Martins <robson@nuuv.net>
2024-06-04 00:41:18 +0000

    updates

--- config.phpmyadmin.inc.php
+++ config.phpmyadmin.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = getenv('MYSQL_HOST', 'localhost');
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
 /* Select mysqli if your server has it */
